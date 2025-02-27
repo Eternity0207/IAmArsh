622fc41730806782f895ad751246fb1aa87ff9cb
Mani <77221000+Eternity0207@users.noreply.github.com>
2025-02-27 16:11:53 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,99 @@
+const logotext = "I Am ARSH";
+const meta = {
+    title: "Arsh Goyal",
+    description: "I’m Arsh Goyal, full stack web developer, currently studying in IIT Jodhpur",
+};
+
+const introdata = {
+    title: "I’m Arsh Goyal",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I do competitive prog.",
+    },
+    description: "I'm a sharp and creative problem-solver who thrives on competitive programming, building sleek websites, and uncovering hidden loopholes.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Who Am I?",
+    aboutme: "A passionate full-stack developer with a knack for competitive programming, web development, and problem-solving. Always exploring new technologies and crafting efficient, user-friendly digital solutions.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Making Personal Projects",
+        where: "IITJ",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "NodeJs",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I create intuitive and visually appealing designs that make digital experiences seamless and enjoyable. With a focus on user needs, I blend aesthetics with functionality to craft interfaces that truly work."
+    },
+    {
+        title: "Website Designing",
+        description: "Custom websites crafted for efficiency, aesthetics, and reliability. Every design is built with precision, ensuring smooth navigation, responsiveness, and a strong online presence."
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "VolunteersIITJ",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "iamarsh0207@gmail.com",
+    YOUR_FONE: "",
+    description: "<strong>Need some help? 🤔</strong> <br>We're here for you! Whether you have questions, need support, or just want to say hello, feel free to reach out. Fill out the form, drop us an email, or connect with us on social media—we’d love to hear from you!",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Eternity0207",
+    instagram: "https://www.instagram.com/sin_master_69/",
+    linkedin: "https://linkedin.com",
+    twitter: "https://x.com/H4CK5R1",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
