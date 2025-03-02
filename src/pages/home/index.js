841dba09fc393dccc5c289a34a82417fd61e68e1
Mani <77221000+Eternity0207@users.noreply.github.com>
2025-03-02 11:16:24 +0530

    Added Undraggable Effect

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -12,7 +12,7 @@ export const Home = () => {
         <Helmet>
           <meta charSet="utf-8" />
           <title> {meta.title}</title>
-          <meta name="description" content={meta.description} />
+          <meta name="description no-drag" content={meta.description} />
         </Helmet>
         <div className="intro_sec d-block d-lg-flex align-items-center ">
           <div
@@ -22,8 +22,8 @@ export const Home = () => {
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
               <div className="intro mx-auto">
-                <h2 className="mb-1x">{introdata.title}</h2>
-                <h1 className="fluidz-48 mb-1x">
+                <h2 className="mb-1x no-drag">{introdata.title}</h2>
+                <h1 className="fluidz-48 mb-1x no-drag">
                   <Typewriter
                     options={{
                       strings: [
@@ -37,7 +37,7 @@ export const Home = () => {
                     }}
                   />
                 </h1>
-                <p className="mb-1x">{introdata.description}</p>
+                <p className="mb-1x no-drag">{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
                     <div id="button_p" className="ac_btn btn ">
